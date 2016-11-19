e8093cd557383aa111817117a124f80dd1af5fd4
hirotaka-tajiri <ganryu_koziro@excite.co.jp>
2016-11-19 15:36:33 +0900

    ssh-agentフォワードの設定を明確化

--- os_package/sshd.rb
+++ os_package/sshd.rb
@@ -1,9 +1,15 @@
 
 # recipe
-template "/etc/ssh/sshd_config" do
-    action :create
-    source "files/sshd_config.template"
-end
+
+[
+    "sshd_config", 
+    "ssh_config"
+].each {| f |
+    template "/etc/ssh/#{f}" do
+        action :create
+        source "files/#{f}.template"
+    end
+}
 
 service "sshd" do
     action :restart
