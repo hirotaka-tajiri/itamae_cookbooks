35682fdc2b6e18f38baa21eef8eb29b3d2ccece8
hirotaka-tajiri <ganryu_koziro@excite.co.jp>
2017-05-26 17:24:39 +0900

    Modify mariadb_gitbacket.sql

--- mariadb/files/mariadb_gitbucket.sql
+++ mariadb/files/mariadb_gitbucket.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `gitbucket` DEFAULT CHARACTER SET `utf8` COLLATE `utf8_unicode_ci`;
 CREATE USER 'git'@'localhost' IDENTIFIED BY 'gitbucket_gitbucket';
 GRANT ALL PRIVILEGES ON `gitbucket`.* TO 'git'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'git'@'localhost';
 DROP DATABASE test;
 FLUSH PRIVILEGES;
